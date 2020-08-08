d8239d1602a8663b300341ffe4cd1526aa15b6bb
Grandmaster M <mitchel.stroosnijder@gmail.com>
2020-08-08 16:45:26 +0200

    fix: env variables

--- util/generateAccessToken.js
+++ util/generateAccessToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const ENV = require('../env');
 
 function generateAccessToken(userId) {
-    return jwt.sign(userId, ENV.JWT_SECRET, { expiresIn: '7d' });
+    return jwt.sign(userId, process.env.JWT_SECRET, { expiresIn: '7d' });
 }
 
 module.exports = generateAccessToken;
